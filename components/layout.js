92bbfb0d186a1622fbfbbb8c31dd4195120c8685
Uday Pratap <udaypratap169@gmail.com>
2020-08-13 21:06:55 +0530

    layout module css file created

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,8 @@
-function Layout({ children }) {
-   return <div>{children}</div>
- }
- 
- export default Layout
+import styles from './layout.module.css'
+
+export default function Layout({ children }) {
+  return <div className={styles.container}>{children}</div>
+}
 
  
  
